f5fbe02b824763f262083dd3498e562a469a9a18
Nico <nicolasaraya@gmail.com>
2021-08-05 10:56:51 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,  {useState} from 'react';
 
 function App() {
+  const [ hour, setHour] = useState('')
+  const hourly = async () => {
+    let url = "api.openweathermap.org/data/2.5/weather?q=Beijing&appid=1f89da47fe4d0be6bbbf376af70bdb58";
+    
+    const requestOptions = {
+      method: 'GET',
+      headers: { "Content-type": "application/json" },
+    }
+
+    const request = await fetch(url, requestOptions);
+    console.log(request, '<--- request')
+    const data = await request.json();
+    console.log(data, '<--- data')
+    setHour(data)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={hourly}></button>
+        <p>{JSON.stringify(hour)}</p>
       </header>
     </div>
   );
